fc292930ec4c2b05e08e7f0a4bc2522612b2667d
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-10-17 15:20:07 +0400

    Saved the filtering parameters in the URL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,91 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import Categories from "../components/Categories";
-import Sort from "../components/Sort";
-import "../scss/app.scss";
+import Sort, { list } from "../components/Sort";
+import qs from "qs";
+import { useNavigate } from "react-router";
 import CarBlock from "../components/CarBlock";
 import Skeleton from "../components/CarBlock/Skeleton";
 import Pagination from "../components/Pagination";
 import { SearchContext } from "../App";
 import { useDispatch, useSelector } from "react-redux";
-import { setCategoryId, setCurrentPage } from "../redux/slices/filterSlice";
+import {
+  setCategoryId,
+  setCurrentPage,
+  setFilters,
+} from "../redux/slices/filterSlice";
 import axios from "axios";
+import "../scss/app.scss";
 
 const Home = () => {
-  const {categoryId,sort,currentPage} = useSelector((state) => state.filter);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isSearch = useRef(false);
+  const isMounted = useRef(false);
+  const { categoryId, sort, currentPage } = useSelector(
+    (state) => state.filter
+  );
   const { searchValue } = useContext(SearchContext);
   // const [currentPage, setCurrentPage] = useState(1);
   const onChangeCategory = (id) => {
     dispatch(setCategoryId(id));
   };
-  const onChangePage = number => {
+  const onChangePage = (number) => {
     dispatch(setCurrentPage(number));
-  }
-  const [isLoading, setIsLoading] = useState(true);
-  const [cars, setCars] = useState([]);
-  
-  useEffect(() => {
+  };
+  const fetchCars = () => {
     setIsLoading(true);
     const order = sort.sortProperty.includes("-") ? "asc" : "desc";
     const sortBy = sort.sortProperty.replace("-", "");
     const category = categoryId > 0 ? `category=${categoryId}` : "";
     const search = searchValue > 0 ? `&search=${searchValue}` : "";
 
-    axios.get(`https://63492c050b382d796c7f6bf1.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`)
-    .then(res => {
-      setCars(res.data);
-      setIsLoading(false);
-});
+    axios
+      .get(
+        `https://63492c050b382d796c7f6bf1.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+      )
+      .then((res) => {
+        setCars(res.data);
+        setIsLoading(false);
+      });
+  };
+  // Если изменились параметры и был первый рендер то делаю это
+  useEffect(() => {
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        sortProperty: sort.sortProperty,
+        categoryId,
+        currentPage,
+      });
+      navigate(`?${queryString}`);
+    }
+    isMounted.current = true;
+  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
+
+  // Если был первый рендер,то проверяю параметры и сохраняю в редуксе
+  useEffect(() => {
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+      const sort = list.find((obj) => obj.sortProperty === params.sortProperty);
+
+      dispatch(
+        setFilters({
+          ...params,
+          sort,
+        })
+      );
+      isSearch.current = true;
+    }
+  }, []);
+  const [isLoading, setIsLoading] = useState(true);
+  const [cars, setCars] = useState([]);
+  
+// Если был первый рендер то запрашиваю автомобили
+  useEffect(() => {
     window.scrollTo(0, 0);
+    if (!isSearch.current) {
+      fetchCars();
+    }
+    isSearch.current = false;
   }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
   const carsItems = cars
