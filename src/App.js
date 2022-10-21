2b720749797b3a6f3bd120bf1ee7f9ebc5c3b8ba
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-10-21 12:39:12 +0400

    <Outlet/>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import { Route, Routes } from "react-router";
-import Header from "./components/Header";
 import NotFoundBlock from "./components/NotFoundBlock";
+import MainLayout from "./layouts/MainLayout";
 import Cart from "./pages/Cart";
+import FullCar from "./pages/FullCar";
 import Home from "./pages/Home";
 import "./scss/app.scss";
 
 function App() {
   return (
-    <div className="wrapper">
-   <Header/>
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFoundBlock />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="car/:id" element={<FullCar />} />
+        <Route path="*" element={<NotFoundBlock />} />
+      </Route>
+    </Routes>
   );
 }
 export default App;
